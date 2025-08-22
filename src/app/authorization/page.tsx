b9708b0293f070fb8d2ec899cfbfcff998a27871
StepanBandera-droid <ribinstepan10@mail.ru>
2025-08-22 21:11:14 +0300

    fix: Changed Authorization page

--- src/app/authorization/page.tsx
+++ src/app/authorization/page.tsx
@@ -1,5 +1,18 @@
 const AuthorizationPage = () => {
-    return <div>Authorization page</div>;
-}
+    return (
+        <div className="authorization-page">
+            <div className="authorization-header">Welcome to the tea shop!</div>
+            <div className="subtitle">Please log in first.</div>
+            <form className="email-form">
+                <input
+                    type="email"
+                    value=""
+                    onChange={(e) => e.target.value}
+                    placeholder="Email"
+                />
+            </form>
+        </div>
+    );
+};
 
 export default AuthorizationPage;
