4549e35f2515e89edb99cde28886e107f2195724
Ivan Rybin <rybin1van@outlook.com>
2025-09-01 13:29:30 +0300

    feat: Added new domain interface, mock products data, Market thunk mocked

--- src/domains/product.ts
+++ src/domains/product.ts
@@ -1,9 +1,49 @@
+export enum BookCoverTypes {
+    HARDCOVER = "hardcover",
+    PAPERBACK = "paperback",
+    DUST_JACKET = "dustJacket",
+    SLIPCASE = "slipcase",
+    LEATHER_BOUND = "leatherBound",
+    DIGITAL = "digital",
+}
+
+export enum Ratings {
+    LOW = 1,
+    SMALL = 2,
+    MEDIUM = 3,
+    GOOD = 4,
+    EXCELLENT = 5,
+}
+
+export interface IReview {
+    author: string;
+    title?: string;
+    text: string;
+    rating: Ratings;
+    date: string;
+    likesCount: number;
+    dislikeCount: number;
+}
+
 export interface IProduct {
     id: string;
     title: string;
-    author: string;
     price: number;
-    description: string;
-    stock: number;
-    createdAt: string;
+    discount: number;
+    isAvailable: boolean;
+    coverType: BookCoverTypes;
+    pagesCount: number;
+    weight: number;
+    annotation: string;
+    publisher: string;
+    publisherBrand: string;
+    buyReasons: string[];
+    ageRating: string;
+    publicationYear: string;
+    ISBN: string;
+    reviews: IReview[];
+    salesCount: number;
+    averageRating: number;
+    ratingsCount: number;
+    imagesUrls: string[];
 }
