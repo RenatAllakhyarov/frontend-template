5b477ac7efdc07c8e2c5f6ec44e5bcc701f94c9d
Ivan Rybin <rybin1van@outlook.com>
2025-09-02 12:40:16 +0300

    fix: MarketPage products fetch listening

--- src/views/MarketPage/index.tsx
+++ src/views/MarketPage/index.tsx
@@ -1,16 +1,34 @@
-import API from "@api/index";
-import ProductsList from "@components/ProductList";
-import { ReactElement } from "react";
+"use client";
 
-const MarketPage = async (): Promise<ReactElement> => {
-    const products = await API.getProducts();
+// import API from "@api/index";
+import ServerError from "@components/ServerError";
+import ProductsList from "@components/ProductList";
+import ProductListSkeleton from "@components/ProductListSkeleton";
+import { fetchProducts } from "@store/slices/Market/thunks";
+import { TAppDispatch, TRootState } from "@store/index";
+import { useDispatch, useSelector } from "react-redux";
+import { ReactElement, useEffect } from "react";
+import "./style.css";
+
+const MarketPage = (): ReactElement => {
+    const dispatch = useDispatch<TAppDispatch>();
+
+    const { isLoading, products, error } = useSelector(
+        (state: TRootState) => state.market
+    );
+
+    useEffect(() => {
+        dispatch(fetchProducts());
+    }, []);
 
     return (
         <div className="market-page">
             <div className="badge badge-text">{"Книги"}</div>
             <h1 className="headline-1-text">{`Книги`}</h1>
 
-            <ProductsList products={products} />
+            {isLoading && <ProductListSkeleton />}
+            {!isLoading && !!error && <ServerError />}
+            {!isLoading && !error && <ProductsList products={products} />}
         </div>
     );
 };
