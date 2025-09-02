2876d70b2183fc06b1ee183d923c1b6886d1558e
Kirill-cyber736 <kirillsidora@yandex.ru>
2025-09-02 12:29:31 +0300

    feat: Added HTTP request with mockData

--- src/components/TransactionInfoPage/index.tsx
+++ src/components/TransactionInfoPage/index.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Cart from "@domains/cart";
+import mockProductsList from "src/mockApi/meta";
 import StyledButton from "@components/StyledButton";
 import { redirect } from "next/navigation";
 import "./style.css"
@@ -12,7 +13,19 @@ const TransactionInfoPage = () => {
         return <div>ERROR</div>
     }
 
-    const cartPrice = cart.getTotalPrice();
+    const mockData = {
+        amount: 1200,
+        userId: 666,
+        products: ["penis", "hui", "pizda"]
+    }
+
+    fetch('http://localhost:3000/transactions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(mockData)
+    })
 
     const transactionDate = Date();
 
@@ -20,7 +33,7 @@ const TransactionInfoPage = () => {
         <div className="transaction-page-container">
             <div>Transaction page</div>
             <div>THE TRANSACTION IS COMPLETED</div>
-            <div>Total price: {cartPrice}</div>
+            <div>Total price: {}</div>
             <div>{transactionDate}</div>
             
             <StyledButton label="Continue shopping" onClick={()=>redirect("/market")}/>
