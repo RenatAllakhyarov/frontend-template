fd4f161342f5ae4c8f22608ca8567f1322ce3aa5
Kirill-cyber736 <kirillsidora@yandex.ru>
2025-09-05 00:00:39 +0300

    fix: Return oposit condition in ApplicationLoadChecker

--- src/components/ApplicationLoadChecker/index.tsx
+++ src/components/ApplicationLoadChecker/index.tsx
@@ -8,7 +8,7 @@ import { ReactNode } from "react";
 const ApplicationLoadChecker = (): ReactNode => {
     const isApplicationLoading = useSelector((state: TRootState) => state.application.isLoading);
 
-    if(isApplicationLoading === false) {
+    if(!isApplicationLoading) {
         return null;
     }
 
