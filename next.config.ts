779e564245d06fd866754a393368d2c1dab88c76
Kirill-cyber736 <kirillsidora@yandex.ru>
2025-09-07 15:21:45 +0300

    feat: Builder config updates for images url using from Moy Sklad

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,27 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
-    reactStrictMode: false,
-    images: {
-        domains: ["miniature-prod.moysklad.ru"],
-    },
+  reactStrictMode: false,
+  images: {
+    domains: ["miniature-prod.moysklad.ru", "api.moysklad.ru"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/products",
+        destination: "http://localhost:3000/products",
+      },
+      {
+        source: "/image-by-url",
+        destination: "http://localhost:3000/image-by-url",
+      },
+      {
+        source: "/external",
+        destination: "http://localhost:3000/external",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
